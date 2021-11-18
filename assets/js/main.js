bc1fb694c3bbb2c4207eb19c9294665421c321c8
LxLorenzo <lorenzocasa1235@gmail.com>
2021-11-18 15:33:48 -0300

    code optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,36 @@
-const menu = document.querySelector('.menu-toggler');
-const nav = document.querySelector('.nav__bar');
-menu.addEventListener('click', () => {
-    menu.classList.toggle("active");
-    nav.classList.toggle("active");
+// MENU HAMBURGUER //
+$('.menu-toggler').on("click", () => {
+    $('.nav__bar').toggleClass('active');
+    $('.menu-toggler').toggleClass('active');
 });
 
-let answers=document.querySelectorAll('.faq__layout');
-answers.forEach((event)=>{
-    event.addEventListener('click',()=>{
-        if(event.classList.contains("active")){
-            event.classList.remove("active");
+//  FAQ //
+$('.faq__layout').each(function () {
+    $(this).on("click", function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
         } else {
-            event.classList.add("active");
+            $(this).addClass('active');
         }
     })
-})
+});
+
+// CLOSE MENU ON CLICK //
+$('.nav__link').on("click", () => {
+    $('.nav__bar').removeClass('active');
+});
+
+// TRANSPARENT MENU WHEN SCROLL DOWN //
+window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('.header').addClass('transparent');
+    }
+    else {
+        $('.header').removeClass('transparent');
+    }
+};
+
+// SCROLL REVEAL //
 
 const sr = ScrollReveal({
     origin: 'right',
@@ -24,41 +40,23 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal('.introduction__description', {origin:'left'});
-sr.reveal('.introduction__title', {origin:'left', delay:300});
-sr.reveal('.introduction__text', {origin:'left', delay:400});
-sr.reveal('.introduction__img', {delay:500});
-sr.reveal('.introduction__button__container', {origin:'left', delay:600})
-sr.reveal('.header', {origin:'top'});
-sr.reveal('.services__title', {origin:'left'})
-sr.reveal('.services__card', {origin:'bottom'})
+sr.reveal('.introduction__description', { origin: 'left' });
+sr.reveal('.introduction__title', { origin: 'left', delay: 300 });
+sr.reveal('.introduction__text', { origin: 'left', delay: 400 });
+sr.reveal('.introduction__img', { delay: 500 });
+sr.reveal('.introduction__button__container', { origin: 'left', delay: 600 })
+sr.reveal('.header', { origin: 'top', reset: false });
+sr.reveal('.services__title', { origin: 'left' })
+sr.reveal('.services__card', { origin: 'bottom' })
 sr.reveal('.knowourwork__img__container');
-sr.reveal('.knowourwork__title', {origin:'left'})
-sr.reveal('.knowourwork__text', {origin:'left'});
-sr.reveal('.faq__title', {origin:'left'});
+sr.reveal('.knowourwork__title', { origin: 'left' })
+sr.reveal('.knowourwork__text', { origin: 'left' });
+sr.reveal('.faq__title', { origin: 'left' });
 sr.reveal('.faq__layout');
-sr.reveal('.contact__title', {origin:'left'});
-sr.reveal('#nome', {origin:'left'});
-sr.reveal('#assunto', {delay: 250});
-sr.reveal('#email', {origin: 'left', delay: 300});
-sr.reveal('#msg', { delay: 350});
-sr.reveal('.contact__button__container', {origin:'bottom'});
-
-var myNav = document.getElementById('header');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        myNav.classList.add("transparent");
-    } 
-    else {
-        myNav.classList.remove("transparent");
-    }
-};
-
-const link = document.querySelector('.nav__link');
-link.addEventListener('click', () => {
-    if (menu.classList.contains("active") && nav.classList.contains("active")) {
-        nav.classList.remove("active");
-        menu.classList.remove("active");
-    }
-});
+sr.reveal('.contact__title', { origin: 'left' });
+sr.reveal('#nome', { origin: 'left' });
+sr.reveal('#assunto', { delay: 250 });
+sr.reveal('#email', { origin: 'left', delay: 300 });
+sr.reveal('#msg', { delay: 350 });
+sr.reveal('.contact__button__container', { origin: 'bottom' });
+sr.reveal('.footer', { origin: 'bottom', reset:false});
